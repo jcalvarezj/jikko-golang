cf8004fc2d07a36bddff2e2793f3bcb7ddd621c4
Juan C. Alvarez <jcalvarezj@gmail.com>
2021-03-13 17:13:39 -0500

    Add 400 response for incorrect/missing data

--- arrays/handlers.go
+++ arrays/handlers.go
@@ -16,23 +16,28 @@ func ArraysHandler(writer http.ResponseWriter, request *http.Request) {
 
 	array := jsonData.Unsorted
 
-	if err != nil {
-		fmt.Println("An error has occurred when parsing JSON data: ", err)
-	} else {
-		original := make([]int, len(array))
-		copy(original, array)
-
-		Mergesort(array)
-		jsonData.Sorted = array
-		jsonData.Unsorted = original
-
-		jsonResponse, err2 := json.Marshal(&jsonData)
-
-		if err2 != nil {
-			fmt.Println("An error has occurred when packing JSON response", err)
+	if len(array) > 0 {
+		if err != nil {
+			fmt.Println("An error has occurred when parsing JSON data: ", err)
 		} else {
-			writer.Header().Set("Content-Type", "application/json; charset=utf-8")
-			writer.Write(jsonResponse)
+			original := make([]int, len(array))
+			copy(original, array)
+
+			Mergesort(array)
+			jsonData.Sorted = array
+			jsonData.Unsorted = original
+
+			jsonResponse, err2 := json.Marshal(&jsonData)
+
+			if err2 != nil {
+				fmt.Println("An error has occurred when packing JSON response", err)
+			} else {
+				writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+				writer.Write(jsonResponse)
+			}
 		}
+	} else {
+		writer.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(writer, "Incorrect or missing data in request body")
 	}
 }
